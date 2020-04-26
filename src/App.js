d0565c63c76e66f06e8ac1038581732d85e0df85
Kamil Sygut <kamil.patryk.sygut@gmail.com>
2020-04-26 18:07:42 +0200

    62. State Design Example: LottoBall Component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
+import Ball from './lotto/Ball'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Ball num={5} />
+            <Ball num={26} />
+            <Ball num={12} />
+            <Ball num={40} />
+        </div>
+    )
 }
 
-export default App;
+export default App
