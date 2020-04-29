3cdb2a53ccd8f491467803ced8d0b5beb6dfd8b6
Kamil Sygut <kamil.patryk.sygut@gmail.com>
2020-04-29 23:04:00 +0200

    125. Requesting a Deck ID

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from "react"
-import Game from "./yahtzee/Game"
+import Deck from "./deck-of-cards/Deck"
 import "./App.css"
 
 class App extends Component {
     render() {
         return (
             <div className='App'>
-                <Game />
+                <Deck />
             </div>
         )
     }
