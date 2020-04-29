961b2cdff0f4c439b5e23d4bfae065f59675a172
Kamil Sygut <kamil.patryk.sygut@gmail.com>
2020-04-29 23:41:24 +0200

    126. Fetching New Cards with AJAX

--- src/deck-of-cards/Deck.js
+++ src/deck-of-cards/Deck.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import axios from 'axios'
 
-const API_URL = 'https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1'
+const API_BASE_URL = 'https://deckofcardsapi.com/api/deck'
 
 class Deck extends Component {
     state = {
@@ -9,14 +9,38 @@ class Deck extends Component {
     }
 
     async componentDidMount() {
-        const deck = await axios.get(API_URL)
-        this.setState({ deck: deck.data })
+        const deck = await axios.get(`${API_BASE_URL}/new/shuffle/`)
+        this.setState({ deck: deck.data, drawn: [] })
+    }
+
+    getCard = async () => {
+        const { deck_id } = this.state.deck
+        const cardUrl = `${API_BASE_URL}/${deck_id}/draw/`
+        try {
+            const cardRes = await axios.get(cardUrl)
+            if (!cardRes.data.success) {
+                throw new Error('No card ramaining!')
+            }
+
+            const card = cardRes.data.cards[0]
+
+            this.setState(state => ({
+                drawn: [...state.drawn, {
+                    id: card.code,
+                    image: card.image,
+                    name: `${card.value} of ${card.suit}`
+                }]
+            }))
+        } catch (error) {
+            alert(error)
+        }
     }
 
     render() {
         return (
             <div>
                 <h1>Card Dealer</h1>
+                <button onClick={this.getCard}>Get Card!</button>
             </div>
         )
     }
