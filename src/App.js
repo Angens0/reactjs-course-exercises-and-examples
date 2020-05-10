02c614fc2cf26ffc7b1e633a0e77d6583f133a8e
Kamil Sygut <kamil.patryk.sygut@gmail.com>
2020-05-10 23:48:44 +0200

    252. Intro to Hooks Project

--- src/App.js
+++ src/App.js
@@ -1,57 +1,10 @@
-import React, { Component } from 'react'
-import Routes from './dogs/Routes'
-import Navbar from './dogs/Navbar'
-import hazel from './img/hazel.jpg'
-import tubby from './img/tubby.jpg'
-import whiskey from './img/whiskey.jpg'
-import './App.css'
+import React from 'react'
+import TodoApp from './hooks-todo/TodoApp'
 
-class App extends Component {
-    static defaultProps = {
-        dogs: [
-            {
-                name: "Whiskey",
-                age: 5,
-                src: whiskey,
-                facts: [
-                    "Whiskey loves eating popcorn.",
-                    "Whiskey is a terrible guard dog.",
-                    "Whiskey wants to cuddle with you!"
-                ]
-            },
-            {
-                name: "Hazel",
-                age: 3,
-                src: hazel,
-                facts: [
-                    "Hazel has soooo much energy!",
-                    "Hazel is highly intelligent.",
-                    "Hazel loves people more than dogs."
-                ]
-            },
-            {
-                name: "Tubby",
-                age: 4,
-                src: tubby,
-                facts: [
-                    "Tubby is not the brightest dog",
-                    "Tubby does not like walks or exercise.",
-                    "Tubby loves eating food."
-                ]
-            }
-        ]
-    }
-
-    render() {
-        return (
-            <div>
-                <Navbar dogs={this.props.dogs} />
-                <div className='container'>
-                    <Routes dogs={this.props.dogs} />
-                </div>
-            </div>
-        )
-    }
+function App() {
+    return (
+        <TodoApp />
+    )
 }
 
 export default App
