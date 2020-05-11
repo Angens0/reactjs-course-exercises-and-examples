961ea9b25b8f418df9c7a08351fd5ada83d724ed
Kamil Sygut <kamil.patryk.sygut@gmail.com>
2020-05-11 15:52:45 +0200

    254. Adding Todo Item Component

--- src/hooks-todo/TodoForm.js
+++ src/hooks-todo/TodoForm.js
@@ -7,13 +7,19 @@ function TodoForm({ addTodo }) {
     const [value, handleChange, reset] = useInputState('')
 
     return (
-        <Paper>
+        <Paper style={{ margin: '1rem 0', padding: '0 1rem' }}>
             <form onSubmit={event => {
                 event.preventDefault()
                 addTodo(value)
                 reset()
             }}>
-                <TextField value={value} onChange={handleChange} />
+                <TextField
+                    value={value}
+                    onChange={handleChange}
+                    margin='normal'
+                    label='Add New Todo'
+                    fullWidth
+                />
             </form>
         </Paper>
     )
