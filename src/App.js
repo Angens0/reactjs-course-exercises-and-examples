1f601627a61a757ae51d028522b949104202ce18
Kamil Sygut <kamil.patryk.sygut@gmail.com>
2020-05-12 18:06:09 +0200

    276. "Hookify-ing" the Rest of the App

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import React, { Component } from 'react'
+import React from 'react'
 import Navbar from './context-demo/Navbar'
 import Form from './context-demo/Form'
 import PageContent from './context-demo/PageContent'
 import { ThemeProvider } from './context-demo/contexts/ThemeContext'
 import { LanguageProvider } from './context-demo/contexts/LanguageContext'
 
-export class App extends Component {
-    render() {
-        return (
-            <ThemeProvider>
-                <LanguageProvider>
-                    <PageContent>
-                        <Navbar />
-                        <Form />
-                    </PageContent>
-                </LanguageProvider>
-            </ThemeProvider>
-        )
-    }
+export const App = props => {
+    return (
+        <ThemeProvider>
+            <LanguageProvider>
+                <PageContent>
+                    <Navbar />
+                    <Form />
+                </PageContent>
+            </LanguageProvider>
+        </ThemeProvider>
+    )
+
 }
 
 export default App
