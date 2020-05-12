199a38444ed368a6fdd5307cfe3665600b44cc40
Kamil Sygut <kamil.patryk.sygut@gmail.com>
2020-05-12 14:57:50 +0200

    266. Intro to Context and Providers

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React, { Component } from 'react'
 import Navbar from './context-demo/Navbar'
 import Form from './context-demo/Form'
+import PageContent from './context-demo/PageContent'
+import { ThemeProvider } from './context-demo/contexts/ThemeContext'
 
 export class App extends Component {
     render() {
         return (
-            <>
-                <Navbar />
-                <Form />
-            </>
+            <ThemeProvider>
+                <PageContent>
+                    <Navbar />
+                    <Form />
+                </PageContent>
+            </ThemeProvider>
         )
     }
 }
